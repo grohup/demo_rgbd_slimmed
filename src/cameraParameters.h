ffdc745773f6d582c4c9377fd2f4e7974caae211
grohbot <grohup@gmail.com>
2018-04-02 16:02:40 -0400

    Running the BAGS

--- src/cameraParameters.h
+++ src/cameraParameters.h
@@ -4,12 +4,12 @@
 #include <opencv/cv.h>
 #include <sensor_msgs/Image.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include "opencv2/contrib/contrib.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+/* #include "opencv2/contrib.hpp"*/
+#include "opencv2/calib3d.hpp"
 
 #include <cv_bridge/cv_bridge.h>
 
